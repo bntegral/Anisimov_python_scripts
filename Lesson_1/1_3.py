e26a92c6282d5617694eb3465f105f1fa74c79d5
bntegral <anisimov.bn@phystech.edu>
2025-04-12 01:48:36 +0300

    Update 1_3.py

--- Lesson_1/1_3.py
+++ Lesson_1/1_3.py
@@ -2,14 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(18, GPIO.OUT)
 
-GPIO.setup(2, GPIO.OUT)
-GPIO.output(2,0)
-time.sleep(3)
-GPIO.output(2,1)
-time.sleep(3)
-GPIO.output(2,0)
-time.sleep(3)
-GPIO.output(2,1)
-time.sleep(3)
-GPIO.output(2,0)
+try:
+    input_value = GPIO.input(17)
+
+    GPIO.output(18, input_value)
+    time.sleep(3)
+
+finally:
+    GPIO.cleanup()
